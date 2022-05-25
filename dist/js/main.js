586b78b2705069c2b350875e926543c71f0ef54c
omar khashaba <66723545+omarsayed12@users.noreply.github.com>
2022-05-26 01:08:43 +0200

    add responsive to pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,3 +14,13 @@ addEventListener('scroll' , ()=> {
     parralaxes.style.backgroundPositionY = (scrollOffset - parralaxes.offsetTop) * .1  + "px"
 
 })
+
+function X() {
+  // Bind Click event to the drop down navigation button
+  document.querySelector('.nav-button').addEventListener('click', function() {
+    /*  Toggle the CSS closed class which reduces the height of the UL thus
+        hiding all LI apart from the first */
+    this.parentNode.parentNode.classList.toggle('closed')
+  }, false);
+}
+X();
