19567cf01e58fa7f644b13995b4fdfeb021c11a9
omar khashaba <66723545+omarsayed12@users.noreply.github.com>
2022-05-26 08:41:23 +0200

    fixed js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,12 @@
-// (function () {
-//     $(".menu-wrapper").on("click", function () {
-//       $(".hamburger-menu").toggleClass("animate");
-//       $(this).toggleClass("bg");
-//       $(".site-wrapper").toggleClass("blur");
-//     });
-//   })();
+/*
+(function () {
+    $(".menu-wrapper").on("click", function () {
+      $(".hamburger-menu").toggleClass("animate");
+      $(this).toggleClass("bg");
+      $(".site-wrapper").toggleClass("blur");
+    });
+ })();
+*/
 
 const selectLang = document.querySelector('.nav-button');
 // function select language
