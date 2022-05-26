af8ac28c453c43fdd68f5852ab43cf6ef6c71dfb
omar khashaba <66723545+omarsayed12@users.noreply.github.com>
2022-05-26 08:32:42 +0200

    adding responsive to about page and do counter

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,42 @@
-(function () {
-    $(".menu-wrapper").on("click", function () {
-      $(".hamburger-menu").toggleClass("animate");
-      $(this).toggleClass("bg");
-      $(".site-wrapper").toggleClass("blur");
-    });
-  })();
+// (function () {
+//     $(".menu-wrapper").on("click", function () {
+//       $(".hamburger-menu").toggleClass("animate");
+//       $(this).toggleClass("bg");
+//       $(".site-wrapper").toggleClass("blur");
+//     });
+//   })();
 
+const selectLang = document.querySelector('.nav-button');
+// function select language
+function changeLabg() {
+  selectLang.addEventListener('click', function() {
+    this.parentNode.parentNode.classList.toggle('closed')
+  }, false);
+}
+changeLabg()
 
 const parralaxes = document.querySelector('.parralax');
 //make a parralax effect
 addEventListener('scroll' , ()=> {
     let scrollOffset = window.scrollY;
     parralaxes.style.backgroundPositionY = (scrollOffset - parralaxes.offsetTop) * .1  + "px"
-
 })
 
-function X() {
-  // Bind Click event to the drop down navigation button
-  document.querySelector('.nav-button').addEventListener('click', function() {
-    /*  Toggle the CSS closed class which reduces the height of the UL thus
-        hiding all LI apart from the first */
-    this.parentNode.parentNode.classList.toggle('closed')
-  }, false);
+function numCounter(tagId,maxCount,speed){
+  var initialNumber = 0;
+  function counter(){
+    document.getElementById(tagId).innerHTML = initialNumber;
+    ++initialNumber;
+  }
+  var counterDelay = setInterval(counter,speed);
+  function totalTime(){
+    clearInterval(counterDelay);
+  }
+  var totalPeriod = speed * (maxCount);
+  setTimeout(totalTime, totalPeriod);
 }
-X();
+numCounter("fCount",15,100);
+numCounter("sCount",4.9,200);
+numCounter("tCount",4,200);
+numCounter("foCount",200,10);
+
